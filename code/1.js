1ff785ef5fa8cc4d72366356c869b9817e5429d0
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-03-09 09:18:55 +0600

    problem 1 solved.

--- code/1.js
+++ code/1.js
@@ -1,1 +1,19 @@
-// 
+/*
+    Rotate array in javascript
+    problem: Given a array and steps(k) value. Rotate the array of n elements to the right by k steps.
+*/
+
+function rotateArray(arr, steps) {
+  if (arr.length < steps) return arr;
+  let newArray = [];
+  for (let i = arr.length - 1; i >= arr.length - steps; i--) {
+    newArray.unshift(arr[i]);
+  }
+  for (let i = 0; i < arr.length - steps; i++) {
+    newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+result = rotateArray([1, 2, 3, 4, 5], 2);
+console.log(result);
